889eb5c3734c186a4b1de14cc4021b9a1fbf0680
Adiom Timur <adiom@canfly.org>
2024-11-25 21:19:48 +0400

    Initial Commit

---
+++ swiftPLP/swiftPLPApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftPLPApp.swift
+//  swiftPLP
+//
+//  Created by adiom on 25.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftPLPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftPLPTests/swiftPLPTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftPLPTests.swift
+//  swiftPLPTests
+//
+//  Created by adiom on 25.11.2024.
+//
+
+import Testing
+@testable import swiftPLP
+
+struct swiftPLPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
